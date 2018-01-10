aefcc3fa9ce08312a007312f44da5a6efc07c2ba
Tia <msksfo@gmail.com>
2018-01-10 15:47:23 -0800

    add contact form. create mobile navigation

--- day-9:toy-or-game/js/script.js
+++ day-9:toy-or-game/js/script.js
@@ -1,3 +1,15 @@
-window.onload = function(){
-	window.scrollTo(0, 0);
-}
+var mobileNavIcon = document.getElementById('mobile-nav-icon');
+var btnClose = document.getElementById('btn-close');
+var sideMenu = document.getElementById('side-menu');
+var mainBanner = document.getElementById('main-banner');
+
+
+mobileNavIcon.addEventListener('click', function(){
+	sideMenu.style.width = '200px';
+	//mainBanner.style.marginLeft = '192px';
+});
+
+btnClose.addEventListener('click', function(){
+	sideMenu.style.width = 0;
+	mainBanner.style.marginLeft = 0;
+});
