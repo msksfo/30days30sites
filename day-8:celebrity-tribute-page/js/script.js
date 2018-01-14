a7e32e3dd58ee17f6c0c1071289238fd197033c7
Tia <msksfo@gmail.com>
2018-01-13 18:59:51 -0800

    adjust position of closing image. add fade in to memoriam text. adjust animation on nav bar to match new timing of js fade in

--- day-8:celebrity-tribute-page/js/script.js
+++ day-8:celebrity-tribute-page/js/script.js
@@ -5,31 +5,31 @@ var standardNav = document.getElementById('standard-nav');
 var navHeight = standardNav.offsetHeight;
 var windowHeight = window.innerHeight;
 
+var reflections = document.getElementById('reflections');
 
 $(document).ready(function(){
-	
-   
 
+	$('.memoriam-text').fadeIn(4000);
+	
     setTimeout(function(){
     	$('#memoriam').fadeOut({
     		duration: 6000,
     		queue: false
     	});
-    }, 2000);
+    }, 4000);
 
 })
 
 
-
 window.addEventListener('scroll', function(){
 
-	if (window.scrollY >= windowHeight - navHeight){
+	if ( (window.scrollY >= windowHeight - navHeight) && (window.scrollY < reflections.offsetTop) ){
 		standardNav.style.backgroundColor = '#010126';
-	} else {
+	}  else {
 		standardNav.style.backgroundColor = 'transparent';
+
 	}
 	
 });
-
 
 
