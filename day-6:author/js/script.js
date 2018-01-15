a1465d542cfad9432b9a2424f0f0332b274a54d3
Tia <msksfo@gmail.com>
2018-01-15 15:55:49 -0800

    update js offsets by subtracting 1. ie quoteOffset = quoteRect.y - 1

--- day-6:author/js/script.js
+++ day-6:author/js/script.js
@@ -5,17 +5,17 @@ var aboutNav = document.getElementById('about-nav');
 
 var booksSection = document.getElementById('books');
 var booksRect = booksSection.getBoundingClientRect();
-var booksOffset = booksRect.y;
+var booksOffset = booksRect.y - 1;
 var booksNav = document.getElementById('books-nav');
 
 var quotesSection = document.getElementById('quotes-section');
 var quoteRect = quotesSection.getBoundingClientRect();
-var quoteOffset = quoteRect.y;
+var quoteOffset = quoteRect.y - 1;
 var quoteNav = document.getElementById('quote-nav');
 
 var artworkSection = document.getElementById('artwork');
 var artworkRect = artworkSection.getBoundingClientRect();
-var artworkOffset = artworkRect.y;
+var artworkOffset = artworkRect.y -1;
 var artworkNav = document.getElementById('artwork-nav');
 
 
@@ -23,9 +23,16 @@ window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 
+/*
+window.onload = function(){
+	window.scrollTo(0, 0);
+}
+*/
 window.addEventListener('scroll', function(e){
 	/*  change the color of the navigation tabs on scroll, to reflect where in the site the user
 	 currently is, similar to bootstrap scrollspy  */
+
+
 
 	var y = this.scrollY;
 
@@ -35,6 +42,9 @@ window.addEventListener('scroll', function(e){
 		quoteNav.classList.remove('quoteHover');
 		artworkNav.classList.remove('artworkHover');
 	} else if ( y >= booksOffset && y < quoteOffset){
+		console.log('scroll books section');
+		console.log(window.scrollY);
+
 		aboutNav.classList.remove('aboutHover');
 		booksNav.classList.add('booksHover');
 		quoteNav.classList.remove('quoteHover');
@@ -58,24 +68,29 @@ window.addEventListener('scroll', function(e){
 currently is */
 
 aboutNav.addEventListener('click', function(){
-	aboutNav.classList.add('aboutHover');
+	//aboutNav.classList.add('aboutHover');
 	booksNav.classList.remove('booksHover');
 	quoteNav.classList.remove('quoteHover');
 	artworkNav.classList.remove('artworkHover');
+	aboutNav.classList.add('aboutHover');
 });
 
 booksNav.addEventListener('click', function(){
+	console.log(booksOffset);
+
 	aboutNav.classList.remove('aboutHover');
-	booksNav.classList.add('booksHover');
+	//booksNav.classList.add('booksHover');
 	quoteNav.classList.remove('quoteHover');
 	artworkNav.classList.remove('artworkHover');
+	booksNav.classList.add('booksHover');
 });
 
 quoteNav.addEventListener('click', function(){
 	aboutNav.classList.remove('aboutHover');
 	booksNav.classList.remove('booksHover');
-	quoteNav.classList.add('quoteHover');
+	//quoteNav.classList.add('quoteHover');
 	artworkNav.classList.remove('artworkHover');
+	quoteNav.classList.add('quoteHover');
 });
 
 artworkNav.addEventListener('click', function(){
