f34a2c28fd7392261d39d192e0f05157c300c14a
Tia <msksfo@gmail.com>
2018-01-21 16:09:02 -0800

    first commit of day 10 project

---
+++ day-10:photographer/js/galleryscript.js
@@ -0,0 +1,36 @@
+/* variables for mobile menu navigation */
+var closeButton = document.getElementById('btn-close');
+var mobileNav = document.getElementById('mobile-nav');
+var mobileNavIcon = document.getElementById('mobile-nav-icon');
+var main = document.getElementById('main');
+
+
+
+/* ========================= event listeners for the mobile navigation ================================ */
+
+
+mobileNavIcon.addEventListener('click', function(){
+	mobileNav.classList.add('display-mobile');
+	main.style.marginTop = '168px';
+});
+
+
+closeButton.addEventListener('click', function(){
+	mobileNav.classList.remove('display-mobile');
+	main.style.marginTop = '2rem';
+});
+
+
+
+/* ======================== event listeners for the galleries ================================  */
+
+
+
+
+
+
+
+
+
+
+
