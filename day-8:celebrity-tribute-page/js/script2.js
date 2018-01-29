a2ad2843ae2d0179ad6919beef47da93fa71fe03
Tia <msksfo@gmail.com>
2018-01-29 08:10:18 -0800

    add js functionality to close mobile nav section when user clicks a link

--- day-8:celebrity-tribute-page/js/script2.js
+++ day-8:celebrity-tribute-page/js/script2.js
@@ -89,6 +89,7 @@ $(document).ready(function(){
    var btnClose = document.getElementById('btn-close');
    var sideMenu = document.getElementById('side-menu');
    var mainBanner = document.getElementById('main-banner');
+   var mobileLinks = document.querySelectorAll('.mobile-link');
 
 
    window.onbeforeunload = function(){
@@ -104,5 +105,30 @@ $(document).ready(function(){
     sideMenu.style.width = 0;
     //mainBanner.style.marginLeft = 0;
    });
+
+   for (let i = 0; i < mobileLinks.length; i++){
+    mobileLinks[i].addEventListener('click', function(){
+      sideMenu.style.width = 0;
+    })
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
