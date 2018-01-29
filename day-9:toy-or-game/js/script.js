3bea6dc1b4bba48085a2a8a8985833d0d88118fd
Tia <msksfo@gmail.com>
2018-01-29 08:25:40 -0800

    add js functionality for mobile nav to close when user clicks a link

--- day-9:toy-or-game/js/script.js
+++ day-9:toy-or-game/js/script.js
@@ -2,6 +2,8 @@ var mobileNavIcon = document.getElementById('mobile-nav-icon');
 var btnClose = document.getElementById('btn-close');
 var sideMenu = document.getElementById('side-menu');
 var mainBanner = document.getElementById('main-banner');
+var mobileLinks = document.querySelectorAll('.mobile-link');
+
 
 
 window.onbeforeunload = function(){
@@ -17,3 +19,9 @@ btnClose.addEventListener('click', function(){
 	sideMenu.style.width = 0;
 	//mainBanner.style.marginLeft = 0;
 });
+
+for (let i = 0; i < mobileLinks.length; i++){
+ mobileLinks[i].addEventListener('click', function(){
+   sideMenu.style.width = 0;
+ });
+}
