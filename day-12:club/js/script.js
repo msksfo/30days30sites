8f9fea5176e00212d50a19c693b3aa3bd0f8d9bb
Tia <msksfo@gmail.com>
2018-02-04 11:11:13 -0800

    first commit of day 12

---
+++ day-12:club/js/script.js
@@ -0,0 +1,78 @@
+var hamburgerIcon = document.getElementById('hamburger-icon');
+var closeButton = document.getElementById('close-button');
+var mobileMenu = document.getElementById('mobile-menu');
+
+var links = document.querySelectorAll('.link');
+
+var mobileLinks = document.querySelectorAll('.mobile-link');
+var subMenus = document.querySelectorAll('.mobile-submenu');
+
+/* event listeners */
+
+hamburgerIcon.addEventListener('click', function(){
+	mobileMenu.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function(){
+	mobileMenu.style.display = 'none';
+	//hideSubmenu();
+});
+
+for (let i = 0; i < links.length; i++){
+	links[i].addEventListener('click', function(){
+		mobileMenu.style.display = 'none';
+	})
+}
+
+/*
+
+for (let i = 0; i < mobileLinks.length; i++){
+	mobileLinks[i].addEventListener('mouseenter', function(){
+				
+		if (this.classList.contains('submenu')){
+			this.nextElementSibling.classList.toggle('hidden');
+		}
+	})
+}
+
+for (let i = 0; i < mobileLinks.length; i++){
+	mobileLinks[i].addEventListener('mouseleave', function(){
+				
+		if (this.classList.contains('submenu')){
+			this.nextElementSibling.classList.toggle('hidden');
+		}
+	})
+}
+*/
+
+function showSubmenu(){
+	hideSubmenu();
+	for (let i = 0; i < mobileLinks.length; i++){
+		
+	}
+}
+
+function hideSubmenu(){
+	for (let i = 0; i < subMenus.length; i ++){
+		subMenus[i].classList.add('hidden');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
